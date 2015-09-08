457380d120f2c7c917cdddcd16ae8b63cb1efdce
davelaursen <davelaursen@gmail.com>
2015-09-08 12:40:24 -0600

    cleaned up themes

--- local.go
+++ local.go
@@ -7,7 +7,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"go/build"
@@ -16,7 +15,6 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"os/user"
 	"path/filepath"
 	"runtime"
 	"strings"
@@ -32,78 +30,17 @@ var repoPath string
 var defaultTheme string
 
 func main() {
-	type Config struct {
-		HTTP         string `json:"http"`
-		OrigHost     string `json:"orighost"`
-		Base         string `json:"base"`
-		Play         bool   `json:"play"`
-		NativeClient bool   `json:"nacl"`
-		Theme        string `json:"theme"`
-		Repo         string `json:"repo"`
-	}
-	// set config instance with default values
-	config := Config{
-		HTTP:         "127.0.0.1:4999",
-		OrigHost:     "",
-		Base:         "",
-		Play:         true,
-		NativeClient: false,
-		Theme:        "",
-		Repo:         "",
-	}
-
-	// ensure config file exists
-	usr, err := user.Current()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not get current user: %s\n", err)
-		fmt.Fprintf(os.Stderr, "Using default settings\n")
-	} else {
-		var configFilePath string
-		if runtime.GOOS == "windows" {
-			configFilePath = filepath.Join(usr.HomeDir, "ppconfig.json")
-		} else {
-			configFilePath = filepath.Join(usr.HomeDir, ".ppconfig")
-		}
-		_, err = os.Stat(configFilePath)
-		if os.IsNotExist(err) {
-			f, err := os.Create(configFilePath)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating file '%s': %s\n", configFilePath, err)
-				os.Exit(1)
-			}
-			defer f.Close()
-
-			f.Write([]byte("{}"))
-			f.Close()
-		}
-
-		// parse config file
-		configFile, err := os.Open(configFilePath)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Unable to open config file '%s': %s\n", configFilePath, err)
-			os.Exit(1)
-		}
-
-		// override defaults with config file values
-		jsonParser := json.NewDecoder(configFile)
-		if err = jsonParser.Decode(&config); err != nil {
-			fmt.Fprintf(os.Stderr, "Unable to parse config file '%s': %s\n", configFilePath, err)
-			os.Exit(1)
-		}
-	}
-
-	// override defaults & config file values with command line values
-	httpAddr := flag.String("http", config.HTTP, "HTTP service address (e.g., '127.0.0.1:4999')")
-	originHost := flag.String("orighost", config.OrigHost, "host component of web origin URL (e.g., 'localhost')")
-	flag.StringVar(&basePath, "base", config.Base, "base path for slide template and static resources")
-	flag.BoolVar(&present.PlayEnabled, "play", config.Play, "enable playground (permit execution of arbitrary user code)")
-	nativeClient := flag.Bool("nacl", config.NativeClient, "use Native Client environment playground (prevents non-Go code execution)")
-	flag.StringVar(&defaultTheme, "theme", config.Theme, "the default theme to apply when no custom styles are defined")
-	flag.StringVar(&repoPath, "repo", config.Repo, "path for theme repository")
+	httpAddr := flag.String("http", "127.0.0.1:4999", "HTTP service address (e.g., '127.0.0.1:4999')")
+	originHost := flag.String("orighost", "", "host component of web origin URL (e.g., 'localhost')")
+	flag.StringVar(&basePath, "base", "", "base path for slide template and static resources")
+	flag.BoolVar(&present.PlayEnabled, "play", true, "enable playground (permit execution of arbitrary user code)")
+	nativeClient := flag.Bool("nacl", false, "use Native Client environment playground (prevents non-Go code execution)")
+	flag.StringVar(&defaultTheme, "theme", "", "the default theme to apply when no custom styles are defined")
+	flag.StringVar(&repoPath, "repo", "", "path for theme repository")
 	flag.Parse()
 
 	if repoPath != "" {
-		_, err = os.Stat(repoPath)
+		_, err := os.Stat(repoPath)
 		if os.IsNotExist(err) {
 			fmt.Fprintf(os.Stderr, "Repo directory '%s' does not exist\n", repoPath)
 			os.Exit(1)
